359d6a6683ada0c20d5b09a6d6a72b3499cafdbf
creaper9487 <w0979563238@gmail.com>
2026-01-15 18:24:38 +0800

    fix: Update Sogan component to emphasize 'HARD' with brackets for better visibility

--- src/components/footer/slogannn.tsx
+++ src/components/footer/slogannn.tsx
@@ -3,7 +3,7 @@ export default function Sogan() {
     <div className="flex text-gray-400 font-extrabold flex-col text-8xl">
     <span>WE ONLY</span>
     <span>SOLVE</span>
-    <span>HARD</span>
+    <span>[HARD]</span>
     <span>PROBLEMS</span>
     </div>
   );
